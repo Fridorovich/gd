055210865855499d739882b756b000f92e6ac101
Egor Nischikh <144816549+Fridorovich@users.noreply.github.com>
2025-02-28 01:13:33 +0500

    lab1

--- src/utils/resource_utils.cpp
+++ src/utils/resource_utils.cpp
@@ -1,51 +1,39 @@
-#include "settings.h"
+//#define STBI_MSC_SECURE_CRT
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+
+#include "resource_utils.h"
 
 #include "utils/error_handler.h"
 
-#include <cxxopts.hpp>
+#include <stb_image_write.h>
 
-using namespace cg;
 
-std::shared_ptr<settings> cg::settings::parse_settings(int argc, char** argv)
+using namespace cg::utils;
+
+std::string view_command(const std::filesystem::path& path)
 {
-	std::shared_ptr<cg::settings> settings = std::make_shared<cg::settings>();
+#ifdef __APPLE__
+	return std::string("open ").append(path.string());
+#endif
+#ifdef _WIN32
+	return std::string("start ").append(path.string());
+#endif
+	return "";
+}
 
-	cxxopts::Options options(argv[0], "Computer graphics in Game development");
+void cg::utils::save_resource(cg::resource<cg::unsigned_color>& render_target, std::filesystem::path filepath)
+{
+	int width = static_cast<int>(render_target.get_stride());
+	int height = static_cast<int>(render_target.count()) / width;
 
-	auto add_options = options.add_options();
-	add_options("height", "Render target height", cxxopts::value<unsigned>()->default_value("1080"));
-	add_options("width", "Render target width", cxxopts::value<unsigned>()->default_value("1920"));
-	add_options("model_path", "Path to OBJ model", cxxopts::value<std::filesystem::path>()->default_value("models/cube.obj"));
-	add_options("camera_position", "Camera position", cxxopts::value<std::vector<float>>()->default_value("0.0,1.0,5.0"));
-	add_options("camera_theta", "Camera polar angle", cxxopts::value<float>()->default_value("0.0"));
-	add_options("camera_phi", "Camera azimuth angle", cxxopts::value<float>()->default_value("0.0"));
-	add_options("camera_angle_of_view", "Camera angle of view", cxxopts::value<float>()->default_value("60.0"));
-	add_options("camera_z_near", "Minimum expected depth", cxxopts::value<float>()->default_value("0.001"));
-	add_options("camera_z_far", "Maximum expected depth", cxxopts::value<float>()->default_value("100.0"));
-	add_options("result_path", "Path to resulted image", cxxopts::value<std::filesystem::path>()->default_value("result.png"));
-	add_options("raytracing_depth", "Maximum number of traces rays", cxxopts::value<unsigned>()->default_value("1"));
-	add_options("accumulation_num", "Number of accumulated frames", cxxopts::value<unsigned>()->default_value("1"));
-	add_options("h,help", "Print usage");
+	int result = stbi_write_png(
+			filepath.string().c_str(), width, height, 3, render_target.get_data(),
+			width * sizeof(cg::unsigned_color));
 
-	auto result = options.parse(argc, argv);
+	if (result != 1)
+		THROW_ERROR("Can't save the resource");
 
-	if (result.count("help"))
-	{
-		THROW_ERROR(options.help());
-	}
-
-	settings->height = result["height"].as<unsigned>();
-	settings->width = result["width"].as<unsigned>();
-	settings->model_path = result["model_path"].as<std::filesystem::path>();
-	settings->camera_position = result["camera_position"].as<std::vector<float>>();
-	settings->camera_theta = result["camera_theta"].as<float>();
-	settings->camera_phi = result["camera_phi"].as<float>();
-	settings->camera_angle_of_view = result["camera_angle_of_view"].as<float>();
-	settings->camera_z_near = result["camera_z_near"].as<float>();
-	settings->camera_z_far = result["camera_z_far"].as<float>();
-	settings->result_path = result["result_path"].as<std::filesystem::path>();
-	settings->raytracing_depth = result["raytracing_depth"].as<unsigned>();
-	settings->accumulation_num = result["accumulation_num"].as<unsigned>();
-
-	return settings;
+	auto command = view_command(filepath);
+	if (!command.empty())
+		std::system(command.c_str());
 }
